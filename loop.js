f311a31114fe720199d6d1b6bc302b82ebea7303
Kalon <kaloncpu57@gmail.com>
2015-11-11 20:41:14 -0700

    Update loop.js

--- loop.js
+++ loop.js
@@ -22,33 +22,55 @@ Element.prototype.setAttributes = function (attrs) {
 
 function createMenuItem(/*type, */label, ariaChecked, func) {
     /*
-  if (type == "checkbox") {
-
-  } else if (type == "popup") {
-
-  }
-  */
-
-    var menuitem = document.createElement("div");
-    menuitem.setAttributes({
-        "class": "ytp-menuitem",
-        "role": "menuitemcheckbox",
-        "tabindex": "39",
-        "aria-checked": ariaChecked,
-        "html": "<div class='ytp-menuitem-label'>" + label + "</div><div class='ytp-menuitem-content'><div class='ytp-menuitem-toggle-checkbox'></div></div>"
-    });
-    var temp = func.toString();
-    temp = temp.substring(temp.indexOf('{')+1,temp.lastIndexOf('}'));
-    func = new Function('"true"==this.getAttribute("aria-checked")?this.setAttribute("aria-checked","false"):this.setAttribute("aria-checked","true");' + temp);
-    menuitem.addEventListener("click", func);
-    document.querySelector("#ytp-main-menu-id").appendChild(menuitem);
+    if (type == "checkbox") {
+        //
+    } else if (type == "popup") {
+        //
+    }
+    */
+    var id = "custom-menu-item-" + label.toLowerCase();
+    if (!document.getElementById(id)) {
+        var menuitem = document.createElement("div");
+        menuitem.setAttributes({
+            "id": id,
+            "class": "ytp-menuitem",
+            "role": "menuitemcheckbox",
+            "tabindex": "39",
+            "aria-checked": ariaChecked,
+            "html": "<div class='ytp-menuitem-label'>" + label + "</div><div class='ytp-menuitem-content'><div class='ytp-menuitem-toggle-checkbox'></div></div>"
+        });
+        var temp = func.toString();
+        temp = temp.substring(temp.indexOf('{')+1,temp.lastIndexOf('}'));
+        func = new Function('"true"==this.getAttribute("aria-checked")?this.setAttribute("aria-checked","false"):this.setAttribute("aria-checked","true");' + temp);
+        menuitem.addEventListener("click", func);
+        document.querySelector("#ytp-main-menu-id").appendChild(menuitem);
+    }
 }
 
-createMenuItem("Loop", document.querySelector("#player-api").querySelector("video").loop?"true":"false", function () {
-    var vid = document.querySelector("#player-api").querySelector("video");
-    if (this.getAttribute("aria-checked") == "true") {
-        vid.loop = true;
-    } else {
-        vid.loop = false;
+function addLoop() {
+    createMenuItem("Loop", document.querySelector("#player-api").querySelector("video").loop?"true":"false", function () {
+        var vid = document.querySelector("#movie_player").querySelector("video");
+        if (this.getAttribute("aria-checked") == "true") {
+            vid.loop = true;
+        } else {
+            vid.loop = false;
+        }
+    });
+}
+
+addLoop();
+document.body.addEventListener("transitionend", function (e) {
+    //console.log(e.target);
+    if (e.target.getAttribute("class").indexOf("ytp-player-content") != -1) {
+        return false;
     }
+    addLoop();
 });
+var observer = new MutationObserver(function (mutations) {
+    mutations.forEach(function (mutation) {
+        if (mutation.attributeName == "src") {
+            addLoop();
+        }
+    });
+});
+observer.observe(document.querySelector("#movie_player").querySelector("video"), {attributes: true});
